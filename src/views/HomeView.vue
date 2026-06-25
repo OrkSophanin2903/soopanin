d76eb5d2dc242a5ddefacd04be62cca45d145dd7
ORK Sophanin <sophanin.pretty@gmail.com>
2026-06-25 15:08:49 +0700

    set up
    Signed-off-by: ORK Sophanin <sophanin.pretty@gmail.com>

---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Sorry, currently unavailable.</div>
+  <!-- <div
+    class="grid min-h-screen grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-8 lg:grid-rows-9 lg:h-screen lg:min-h-0"
+  >
+    <div class="min-h-40 rounded-lg border lg:min-h-0 lg:col-span-2 lg:row-span-5"></div>
+    <div
+      class="min-h-40 rounded-lg border lg:min-h-0 lg:col-span-2 lg:row-span-4 lg:col-start-1 lg:row-start-6"
+    ></div>
+    <div
+      class="min-h-40 rounded-lg border md:col-span-2 lg:min-h-0 lg:col-span-4 lg:row-span-3 lg:col-start-3 lg:row-start-1"
+    ></div>
+    <div
+      class="min-h-40 rounded-lg border lg:min-h-0 lg:col-span-2 lg:row-span-3 lg:col-start-7 lg:row-start-1"
+    ></div>
+    <div
+      class="min-h-40 rounded-lg border lg:min-h-0 lg:col-span-2 lg:row-span-3 lg:col-start-7 lg:row-start-4"
+    ></div>
+    <div
+      class="min-h-40 rounded-lg border lg:min-h-0 lg:col-span-2 lg:row-span-3 lg:col-start-5 lg:row-start-4"
+    ></div>
+    <div
+      class="min-h-40 rounded-lg border md:col-span-2 lg:min-h-0 lg:col-span-4 lg:row-span-3 lg:col-start-5 lg:row-start-7"
+    ></div>
+    <div
+      class="min-h-40 rounded-lg border lg:min-h-0 lg:col-span-2 lg:row-span-6 lg:col-start-3 lg:row-start-4"
+    ></div>
+  </div> -->
+</template>
